ec893bde0fb2faf0e3939b3dc5d463fa8b4ff58e
jemin <rudanijemin18@gmail.com>
2021-04-22 13:46:23 +0530

    input models

---
+++ store/models/product.py
@@ -0,0 +1,9 @@
+from django.db import models  
+ 
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price=models.IntegerField(default=0)
+    description=models.CharField(max_length=280,default=" ")
+    image=models.ImageField(upload_to="products/")
