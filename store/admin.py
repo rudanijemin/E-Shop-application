230570426fe399469bc3ff5d145629cfd4d172bc
jemin <rudanijemin18@gmail.com>
2021-04-22 14:14:46 +0530

    add product

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models.product import Product
 # Register your models here.
+admin.site.register(Product)
--- store/models/product.py
+++ store/models/product.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price=models.IntegerField(default=0)
     description=models.CharField(max_length=280,default=" ")
-    image=models.ImageField(upload_to="products/")
+    image=models.ImageField(upload_to="uploads/products/")
