7361a97cf26519c23b45eabbb34f052e883a682c
jemin <rudanijemin18@gmail.com>
2021-05-08 21:37:17 +0530

    listing category in home page

--- store/models/category.py
+++ store/models/category.py
@@ -3,5 +3,10 @@ from django.db import models
 class category(models.Model):
     name = models.CharField(max_length=50)
 
+    
+    @staticmethod
+    def get_all_categories():
+        return category.objects.all()
+
     def __str__(self): 
         return self.name
--- store/views.py
+++ store/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,HttpResponse
 from .models.product import Product
+from .models.category import category
 
 
 # Create your views here.
 def index(request):
-    Prds=Product.get_all_products();
-    
-    return render(request,"index.html",{"products":Prds})
+    Products=Product.get_all_products();
+    categories=category.get_all_categories();
+    # return render(request  ,"order/order.html"); 
+    data = {}
+    data["products"]=Products
+    data["categories"]=categories
+    return render(request,"index.html",data)
